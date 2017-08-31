440ee0993999d9ec41de50cbe3f648fb5c865a42
Felix <felix.p.magnus@gmail.com>
2017-08-31 12:30:45 +0200

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+
+        name: 'main',
+        components: {},
+        data () {
+            return {
+
+            }
+        },
+        created: function () {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
